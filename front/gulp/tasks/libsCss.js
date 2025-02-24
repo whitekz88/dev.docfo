97aaa919dcfb0c4aa4168b09347d7359361573ef
whitekz88 <job.beloshapkin@gmail.com>
2025-02-24 13:47:50 +0500

    Удалил папку с фронтом

--- front/gulp/tasks/libsCss.js
+++
@@ -1,9 +0,0 @@
-import clean_css from 'gulp-clean-css' // чистит и сжимает css
-export const libsCss = () => {
-  return app.gulp
-    .src(app.path.src.wowCss)
-    .pipe(app.plugins.concat('libs.min.css'))
-    .pipe(app.plugins.if(app.isBuild, clean_css()))
-    .pipe(app.gulp.dest(app.path.build.css))
-    .pipe(app.plugins.browsersync.stream())
-}
